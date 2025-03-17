8246593fbad753c79779177f77461b546d6f2a29
tomhebcit <the18@my.bcit.ca>
2025-03-17 14:05:18 -0700

    added 2 more test poses

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import PoseDetection from "./MyPoseDetection"; // Your tracking component
 import PoseDetectionFromImage from "./PoseDetectionFromImage";
 import tree_pose from "./poses/tree_pose.jpeg"; // Example reference image
 import warrior_pose from "./poses/warrior_pose.png"; // Example reference image
+import side_stretch from "./poses/side_stretch.png"; // Example reference image
+import figure_4 from "./poses/figure_4.png"; // Example reference image
 import "./App.css";
 
 const yogaPoses = [
   { name: "Tree Pose", value: "tree_pose", image: tree_pose},
-  { name: "Warrior Pose", value: "warrior_pose", image: warrior_pose }
+  { name: "Warrior Pose", value: "warrior_pose", image: warrior_pose },
+  { name: "Side Stretch", value: "side_stretch", image: side_stretch },
+  { name: "Figure 4", value: "figure_4", image: figure_4 },
 ];
 
 const App = () => {
